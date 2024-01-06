56aa424f24444d869997d43a30fe79d03611ea3a
kim <rlaalsrbs15@naver.com>
2024-01-07 00:18:26 +0900

    분수 합

--- 백준/분수 합.py
+++ 백준/분수 합.py
@@ -7,6 +7,6 @@ def gcd(x, y):
 A1, B1 = map(int, input().split())
 A2, B2 = map(int, input().split())
 
-x, y = A1*B2+A2*B1, B1*B2
-z = gcd(A1*B2+A2*B1, B1*B2)
+x, y = A1*B2+A2*B1, B1*B2 # x = 분자 y = 분모
+z = gcd(x, y)
 print(x//z, y//z)
