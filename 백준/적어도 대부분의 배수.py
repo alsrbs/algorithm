986e933db8f05897f1cde8c3bd9db73ecac7863b
kim <rlaalsrbs15@naver.com>
2024-01-14 21:54:06 +0900

    적어도 대부분의 배수

---
+++ 백준/적어도 대부분의 배수.py
@@ -0,0 +1,13 @@
+nums = sorted(map(int, input().split()))
+result = min(nums)
+while True:
+    c = 0
+    for i in range(5):
+        if result%nums[i]==0:
+            c+=1
+    if c>=3:
+        print(result)
+        break
+    result += 1
+
+
