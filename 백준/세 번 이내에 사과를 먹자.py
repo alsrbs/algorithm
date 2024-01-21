88373500a9d1aca64de6371f75e1c245c3ffee01
kim <rlaalsrbs15@naver.com>
2024-01-21 23:06:01 +0900

    너의 평점은

--- 백준/세 번 이내에 사과를 먹자.py
+++ 백준/세 번 이내에 사과를 먹자.py
@@ -4,33 +4,37 @@ input = sys.stdin.readline
 
 dy = [0, 1, 0, -1]
 dx = [1, 0, -1, 0]
+
+def dfs(x, y, apple, distance):
+    global res
+    if distance <= 3 and apple >= 2:
+        res = 1
+        return
+
+    elif distance == 3 and apple < 2:
+        return
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < 5 and 0 <= ny < 5:
+            tmp = graph[x][y]
+            graph[x][y] = -1
+
+            if graph[nx][ny] == 0:
+                dfs(nx, ny, apple, distance+1)
+
+            elif graph[nx][ny] == 1:
+                dfs(nx, ny, apple+1, distance+1)
+
+            graph[x][y] = tmp
+
 graph = [list(map(int, input().split())) for _ in range(5)]
 visited = [[False] * 5 for _ in range(5)]
 r, c = map(int, input().split())
+res = 0
+dfs(r, c, 0, 0)
+print(res)
 
-stack = [(r, c, 0, 0)]  # 스택에 시작 상태 추가
 
-while stack:
-    y, x, depth, cnt = stack.pop()
-    visited[y][x] = True
-    if graph[y][x] == 1:
-        cnt += 1
-
-    if 2 <= cnt:
-        print(1)
-        break
-
-    if 3 == depth:
-        visited[y][x] = False
-        continue
-
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-
-        if 0 <= ny < 5 and 0 <= nx < 5:
-            if not visited[ny][nx] and graph[ny][nx] != -1:
-                stack.append((ny, nx, depth + 1, cnt))
-
-else:
-    print(0)
