eee496901f04881153470903e6371c62940bb777
kim <rlaalsrbs15@naver.com>
2024-02-11 23:14:21 +0900

    N번째 큰 수

---
+++ 백준/N번째 큰 수.py
@@ -0,0 +1,22 @@
+import sys, heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+q = []
+
+for _ in range(n):
+    nums = list(map(int, input().split()))
+
+    if not q:
+        for num in nums:
+            heapq.heappush(q, num)
+
+    else:
+        for num in nums:
+            if q[0] < num:
+                heapq.heappush(q, num)
+                heapq.heappop(q)
+
+print(q[0])
+
