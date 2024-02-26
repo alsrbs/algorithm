d09db3921ffa57038aea167115f683d6ca1a9817
kim <rlaalsrbs15@naver.com>
2024-02-26 09:13:15 +0900

    양

--- 백준/가장 가까운 세 사람의 심리적 거리.py
+++ 백준/가장 가까운 세 사람의 심리적 거리.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
@@ -6,31 +7,29 @@ input = sys.stdin.readline
 def mbti_distance():
     global result
 
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                tmp = 0
-                if i == j or j == k or i == k:continue
+    for i in perm:
+        tmp = 0
+        if sorted(i) in mbti_list:continue
+        for x in range(4):
+            if i[0][x] != i[1][x]: tmp += 1
+            if i[0][x] != i[2][x]: tmp += 1
+            if i[1][x] != i[2][x]: tmp += 1
 
-                for x in range(4):
-                    if mbti[i][x] != mbti[j][x]: tmp += 1
-                    if mbti[j][x] != mbti[k][x]: tmp += 1
-                    if mbti[i][x] != mbti[k][x]: tmp += 1
-                result = min(tmp, result)
-                if result == 0:
-                    return 0
+        mbti_list.append(sorted(i))
+        result = min(tmp, result)
+        if result == 0:
+            return 0
 
 
 for _ in range(int(input())):
     n = int(input())
     mbti = list(input().split())
     result = 9999999
-
+    mbti_list = []
     if n > 32:
         print(0)
 
     else:
+        perm = list(combinations(mbti, 3))
         mbti_distance()
         print(result)
-
-
