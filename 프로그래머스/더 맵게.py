b1e5289ea8987d4a25b5cfb4dc14d8e8974805ee
kim <rlaalsrbs15@naver.com>
2024-02-28 16:03:44 +0900

    더 맵게

---
+++ 프로그래머스/더 맵게.py
@@ -0,0 +1,21 @@
+import heapq
+
+scoville = [1, 2, 3, 9, 10, 12]
+k = 7
+
+heapq.heapify(scoville)
+result = 0
+while True:
+
+    if len(scoville) == 1 and scoville[0] < k:
+        result = -1
+        break
+    if scoville[0] >= k:
+        break
+    x1 = heapq.heappop(scoville)
+    x2 = heapq.heappop(scoville)
+    heapq.heappush(scoville, x1+x2*2)
+
+    result += 1
+    
+print(result)
