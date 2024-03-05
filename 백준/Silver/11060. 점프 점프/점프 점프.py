31b06deabf25d9a4835b0d8f5aa250ba4f33bbc3
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-03-06 04:54:57 +0900

    [Silver II] Title: 점프 점프, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.py
+++ 백준/Silver/11060. 점프 점프/점프 점프.py
@@ -1,30 +1,18 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
 
-
+# DP
 n = int(input())
 a_list = list(map(int, input().split()))
+dp = [n+1]*n
+dp[0] = 0
 
-if n == 1:
-    print(0)
-else:
-    vis = [0]*(n+1)
-    q = deque([1])
+for i in range(n):
+    for j in range(1, a_list[i] + 1):
+        if i + j < n:
+            dp[i+j] = min(dp[i] + 1, dp[i+j])
 
-    while q:
-        idx = q.popleft()
-
-        if idx + a_list[idx - 1] >= n:
-            print(vis[idx]+1)
-            exit()
-
-        for i in range(1, a_list[idx - 1] + 1):
-            new_idx = i + idx
-
-            if vis[new_idx] == 0:
-                vis[new_idx] = vis[idx] + 1
-                q.append(new_idx)
-
+if dp[n - 1] == n + 1:
     print(-1)
+else:
+    print(dp[n - 1])
