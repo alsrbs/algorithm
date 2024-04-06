54f42241cd67f9cfac4baa4d6316fd704a0a650f
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-04-07 01:59:32 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,15 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if ineq == '>' and eq == '=':
+        if n >= m:
+            return 1
+    if ineq == '>' and eq == '!':
+        if n > m:
+            return 1
+    if ineq == '<' and eq == '=':
+        if n <= m:
+            return 1
+    if ineq == '<' and eq == '!':
+        if n < m:
+            return 1
+    return answer
