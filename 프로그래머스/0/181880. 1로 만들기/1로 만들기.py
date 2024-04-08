7b5c560a05458fbc4b704f46bfb14af79d92f76e
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-04-08 19:12:47 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,9 @@
+def solution(num_list):
+    answer = 0
+    for i in num_list:
+        while i != 1:
+            if i%2 == 1:
+                i -= 1
+            i //= 2
+            answer += 1
+    return answer
