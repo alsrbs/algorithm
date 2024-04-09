a04c19e83ba82004ec772915717249549ddf3905
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-04-10 02:46:32 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
@@ -1,5 +1,2 @@
 def solution(my_string, index_list):
-    answer = ''
-    for idx in index_list:
-        answer += my_string[idx]
-    return answer
+    return ''.join(my_string[idx] for idx in index_list)
