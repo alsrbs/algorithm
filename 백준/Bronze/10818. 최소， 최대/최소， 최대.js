98d0b8152484ca17c61fdffd745c3fdcfe250406
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-05-07 17:28:14 +0900

    [Bronze III] Title: 최소, 최대, Time: 436 ms, Memory: 93656 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let nums = input[1].split(' ').map(Number);
-nums.sort((a, b) => a - b);
-console.log(nums[0], nums[input[0] - 1])
+console.log(Math.min.apply(null, nums), Math.max.apply(null, nums))
+
