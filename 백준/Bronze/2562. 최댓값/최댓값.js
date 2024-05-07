142a8a138c9b6f5ba92f0cc055be0ed6be95e120
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-05-07 17:45:21 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,7 +1,13 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
+let [answer, idx] = [0, 0];
 let nums = input.map(Number)
-let Max_num = Math.max.apply(null, nums)
-console.log(Max_num)
-console.log(nums.indexOf(Max_num) + 1)
+for(let i = 0; i <= 8; i++) {
+  if (answer < nums[i]) {
+    answer = nums[i];
+    idx = i;
+  }
+}
+console.log(answer)
+console.log(idx + 1)
