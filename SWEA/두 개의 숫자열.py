79295dce2e5b187ccf7b86c2d06cc26a3ca937ce
kim <rlaalsrbs15@naver.com>
2024-06-06 19:01:24 +0900

    두 개의 숫자열

---
+++ SWEA/두 개의 숫자열.py
@@ -0,0 +1,17 @@
+for t in range(1, int(input())+1):
+    print(f'#{t}', end=' ')
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    B_list, S_list = [], []
+    if n>m:B_list, S_list = a, b
+    else:B_list, S_list = b, a
+
+    result = -9999
+    for i in range(max(n, m)-min(n, m)+1):
+        total = 0
+        for j in range(min(n, m)):
+            total += B_list[i+j]*S_list[j]
+        result = max(result, total)
+    print(result)
