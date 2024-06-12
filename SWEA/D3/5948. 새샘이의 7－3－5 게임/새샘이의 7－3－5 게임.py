c353e0ba8eb6d1cc929d84a1f78727a1b8dc5af9
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-06-13 02:23:25 +0900

    [D3] Title: 새샘이의 7-3-5 게임, Time: 1,307 ms, Memory: 51,960 KB -BaekjoonHub

--- SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.py
+++ SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.py
@@ -1,9 +1,9 @@
-from itertools import permutations
-
 for t in range(1, int(input())+1):
     print(f'#{t}', end=' ')
-    nums = list(map(int, input().split()))
-    perm = list(permutations(nums, 3))
-    result = sorted(set(sum(i) for i in perm))
-    print(result[-5])
-
+    nums = sorted(map(int, input().split()), reverse=True)
+    result = set()
+    for i in range(5):
+        for j in range(i+1, 7):
+            for k in range(j+1, 7):
+                result.add(nums[i]+nums[j]+nums[k])
+    print(sorted(result)[-5])
