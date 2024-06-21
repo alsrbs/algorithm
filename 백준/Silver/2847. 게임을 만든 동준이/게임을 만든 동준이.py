dfb69e8d68c2181c0da59c5eb94b83618e950709
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-06-22 02:49:23 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+ans = 0
+for i in range(n-1, 0, -1):
+    if arr[i] <= arr[i-1]:
+        x = arr[i-1] - arr[i] + 1
+        ans += x
+        arr[i-1] -= x
+
+print(ans)
