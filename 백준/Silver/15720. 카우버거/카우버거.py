01539c6bb45a5040068b8ea8af810bccd4180bd4
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-06-23 00:31:33 +0900

    [Silver V] Title: 카우버거, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15720. 카우버거/카우버거.py
@@ -0,0 +1,36 @@
+a, b, c = map(int, input().split())
+Burger = sorted(map(int, input().split()), reverse=True)
+side = sorted(map(int, input().split()), reverse=True)
+beverage = sorted(map(int, input().split()), reverse=True)
+
+ans = 0
+B, s, bev = 0, 0, 0
+while True:
+    if B >= a and s >= b and bev >= c:
+        break
+
+    B_m, s_m, bev_m = 0, 0, 0
+    if B >= a:
+        B_m = 0
+    else:
+        B_m = Burger[B]
+
+    if s >= b:
+        s_m = 0
+    else:
+        s_m = side[s]
+
+    if bev >= c:
+        bev_m = 0
+    else:
+        bev_m = beverage[bev]
+
+    B, s, bev = B+1, s+1, bev+1
+
+    if 0 not in (B_m, s_m, bev_m):
+        ans += B_m+s_m+bev_m - (B_m+s_m+bev_m)*0.1
+    else:
+        ans += B_m+s_m+bev_m
+
+print(sum(Burger) + sum(side) + sum(beverage))
+print(int(ans))
