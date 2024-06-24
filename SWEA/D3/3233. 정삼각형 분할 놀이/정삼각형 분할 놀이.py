f2643ba729a14d44ed4ebe2d952e9c41892cbddd
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-06-25 04:04:42 +0900

    [D3] Title: 정삼각형 분할 놀이, Time: 497 ms, Memory: 50,552 KB -BaekjoonHub

--- SWEA/D3/3233. 정삼각형 분할 놀이/정삼각형 분할 놀이.py
+++ SWEA/D3/3233. 정삼각형 분할 놀이/정삼각형 분할 놀이.py
@@ -1,7 +1,7 @@
-def extent(x):
-    return 3**2/1/4*x**2
-
 for t in range(1, int(input())+1):
     print(f'#{t}', end=' ')
     a, b = map(int, input().split())
-    print(int(extent(a)//extent(b)))
+    ans = 0
+    for i in range(a//b):
+        ans += i*2 + 1
+    print(ans)
