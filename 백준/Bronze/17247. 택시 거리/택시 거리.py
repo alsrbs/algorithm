bdfa309b247487d4683fc6a040f16900020ed3d2
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-06-27 12:30:47 +0900

    [Bronze I] Title: 택시 거리, Time: 168 ms, Memory: 36576 KB -BaekjoonHub

---
+++ 백준/Bronze/17247. 택시 거리/택시 거리.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+ab_point = []
+
+for i in range(n):
+    if 1 in arr[i]:
+        for j in range(m):
+            if arr[i][j] == 1:
+                ab_point.append((i, j))
+
+print(abs(ab_point[1][0] - ab_point[0][0]) + abs(ab_point[1][1] - ab_point[0][1]))
