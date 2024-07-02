ee8ca0d4812473887ad1ab79f44cc45814955d90
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-02 16:11:35 +0900

    [Silver II] Title: 방탈출, Time: 472 ms, Memory: 49740 KB -BaekjoonHub

--- 백준/Silver/15729. 방탈출/방탈출.py
+++ 백준/Silver/15729. 방탈출/방탈출.py
@@ -3,16 +3,15 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
-current = [0] * n
 
 ans = 0
 for i in range(n):
-    if arr[i] != current[i]:
-        current[i] = not current[i]
+    if arr[i] == 1:
+        arr[i] = 1 - arr[i]
         if i+1 < n:
-            current[i+1] = not current[i+1]
+            arr[i+1] = 1 - arr[i+1]
         if i+2 < n:
-            current[i+2] = not current[i+2]
+            arr[i+2] = 1 - arr[i+2]
         ans += 1
 
 print(ans)
