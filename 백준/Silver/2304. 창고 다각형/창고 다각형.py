306ed8106dfb9490b8213dc01a61491b6af10730
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-03 20:00:05 +0900

    [Silver II] Title: 창고 다각형, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 storage = []
@@ -8,12 +11,18 @@ for _ in range(n):
 storage.sort()
 
 # 가장 높은 기둥의 높이와 위치를 찾는다.
-max_h = max(storage, key=lambda x: x[1])[1]
-max_idx = next(i for i, v in enumerate(storage) if v[1] == max_h)
+max_h = 0
+max_idx = 0
+for i in range(n):
+    if storage[i][1] > max_h:
+        max_h = storage[i][1]
+        max_idx = i
+
+# 가장 높은 기둥의 넓이 추가
+ans = max_h
 
 # 왼쪽 부분 계산
 h = storage[0][1]
-ans = 0
 for i in range(max_idx):
     if h < storage[i + 1][1]:
         ans += h * (storage[i + 1][0] - storage[i][0])
@@ -29,8 +38,5 @@ for i in range(n - 1, max_idx, -1):
         h = storage[i - 1][1]
     else:
         ans += h * (storage[i][0] - storage[i - 1][0])
-
-# 가장 높은 기둥의 넓이 추가
-ans += max_h
 
 print(ans)
