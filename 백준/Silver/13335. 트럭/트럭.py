3bf4c963802f7cc0a48b0a25afa5530f088d5519
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-04 00:49:56 +0900

    [Silver I] Title: 트럭, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -3,12 +3,15 @@ truck = list(map(int, input().split()))
 
 bridge = [0]*w
 ans = 0
+s = 0
 while bridge:
     ans += 1
-    bridge.pop(0)
+    s -= bridge.pop(0)
     if truck:
-        if sum(bridge) + truck[0] <= l:
-            bridge.append(truck.pop(0))
+        if s + truck[0] <= l:
+            x = truck.pop(0)
+            s += x
+            bridge.append(x)
         else:
             bridge.append(0)
 
