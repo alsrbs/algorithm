bdffe60b0d1864b19c4d5b4573a87a526ea3a1f5
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-03 18:37:31 +0900

    [Silver IV] Title: 스택 2, Time: 1292 ms, Memory: 71112 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+stack = []
+for _ in range(n):
+    cmd = list(map(int, input().split()))
+
+    if cmd[0] == 1:
+        stack.append(cmd[1])
+
+    elif cmd[0] == 2:
+        if stack:
+            num = stack.pop()
+            print(num)
+        else:
+            print(-1)
+
+    elif cmd[0] == 3:
+        print(len(stack))
+
+    elif cmd[0] == 4:
+        if stack:
+            print(0)
+        else:
+            print(1)
+
+    else:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
