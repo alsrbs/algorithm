0456062f4a2c1a74db7011d400f1aee7a19a40be
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-03 17:48:21 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 1204 ms, Memory: 87412 KB -BaekjoonHub

---
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -0,0 +1,24 @@
+import sys
+input =sys.stdin.readline
+
+n = int(input())
+
+stack = []
+ans = 0
+for _ in range(n):
+    Homework = list(map(int, input().split()))
+
+    if Homework[0] == 1:
+        if Homework[2] == 1:
+            ans += Homework[1]
+        else:
+            stack.append([Homework[1], Homework[2]-1])
+        continue
+
+    if stack:
+        stack[-1][1] -= 1
+        if stack[-1][1] == 0:
+            h = stack.pop()
+            ans += h[0]
+
+print(ans)
