6e15a5c666f6dba6d9528d3605e6c49b29c883bc
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-05 05:34:39 +0900

    [Silver IV] Title: 유통기한, Time: 808 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/26083. 유통기한/유통기한.py
+++ 백준/Silver/26083. 유통기한/유통기한.py
@@ -1,27 +1,18 @@
 import sys
-
 input = sys.stdin.readline
 
 
-def checkDate(li):
-    data = []
-    if 0 < li[1] <= 12:
-        if 0 < li[2] <= monthCheck(li[0], li[1]):
-            data.append(li)
-    if 0 < li[1] <= 12:
-        if 0 < li[0] <= monthCheck(li[2], li[1]):
-            data.append([li[2], li[1], li[0]])
-    if 0 < li[0] <= 12:
-        if 0 < li[1] <= monthCheck(li[2], li[0]):
-            data.append([li[2], li[0], li[1]])
-    return data
+def deadline_check(y, m, d):
+    if today > [y, m, d]:
+        return "unsafe"
+    return "safe"
 
 
 def monthCheck(year, month):
     if month in [1, 3, 5, 7, 8, 10, 12]:
         return 31
     elif month == 2:
-        if not year % 4:
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
             return 29
         else:
             return 28
@@ -31,23 +22,21 @@ def monthCheck(year, month):
 
 today = list(map(int, input().split()))
 n = int(input())
-for i in range(n):
-    date = checkDate(list(map(int, input().split())))
 
-    newDate = []
-    for i in date:
-        if i not in newDate:
-            newDate.append(i)
-    date = newDate
-    if not date:
-        print("invalid")
-    else:
-        safe = 1
-        for j in date:
-            if today > j:
-                safe = 0
+for _ in range(n):
+    deadline = list(map(int, input().split()))
+    ans = 'invalid'
+
+    # 가능한 모든 날짜 형식 (년, 월, 일) 조합에 대해 검사
+    for idx in [(0, 1, 2), (2, 1, 0), (2, 0, 1)]:
+        yy = deadline[idx[0]]
+        mm = deadline[idx[1]]
+        dd = deadline[idx[2]]
+
+        # 유효한 날짜인지 확인
+        if 1 <= mm <= 12 and 1 <= dd <= monthCheck(yy, mm):
+            ans = deadline_check(yy, mm, dd)
+            if ans == 'unsafe':
                 break
-        if safe:
-            print("safe")
-        else:
-            print("unsafe")
+
+    print(ans)
