ba8853d24b94236fb390d9cf5a9e8de6102ec2de
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-10 00:04:24 +0900

    [Silver I] Title: 알고리즘 기말고사, Time: 168 ms, Memory: 47436 KB -BaekjoonHub

---
+++ 백준/Silver/10424. 알고리즘 기말고사/알고리즘 기말고사.py
@@ -0,0 +1,10 @@
+n = int(input())
+ans = [0] * n
+score = list(map(int, input().split()))
+
+# 딕셔너리를 사용하여 값의 인덱스를 저장
+score_index = {value: index + 1 for index, value in enumerate(score)}
+
+for i in range(n):
+    ans[i] = score_index[i + 1]
+    print((i + 1) - ans[i])
