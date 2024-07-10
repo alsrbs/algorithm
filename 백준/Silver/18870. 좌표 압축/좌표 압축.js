1fa2c5889e359733a366849c320df9d533cea4e5
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-10 23:43:49 +0900

    [Silver II] Title: 좌표 압축, Time: 1912 ms, Memory: 316724 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split('\n');
 
 let n = Number(input[0]);
-let nList = input[1].split(' ');
+let nList = input[1].split(' ').map(Number);
 
 // 정렬된 배열에서 중복된 값을 제거하여 고유한 값만 남김
-let arr = [...new Set(nList)].map(Number);
+let arr = [...new Set(nList)];
 
 // arrSort 배열을 오름차순으로 정렬
 arr.sort((a, b) => a - b);
