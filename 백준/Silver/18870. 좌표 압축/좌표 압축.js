538b522a32c70a9cd30d08b465f073f8d03e5cd8
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-10 23:56:16 +0900

    [Silver II] Title: 좌표 압축, Time: 1652 ms, Memory: 301448 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -2,23 +2,23 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split('\n');
 
 let n = Number(input[0]);
-let nList = input[1].split(' ').map(Number);
+let arr = input[1].split(' ').map(Number);
 
 // 정렬된 배열에서 중복된 값을 제거하여 고유한 값만 남김
-let arr = [...new Set(nList)];
+let uniqueArray = [...new Set(arr)];
 
 // arrSort 배열을 오름차순으로 정렬
-arr.sort((a, b) => a - b);
+uniqueArray.sort((a, b) => a - b);
 
 // 각 숫자의 압축된 인덱스를 저장할 객체
-let arrDic = {};
+let myMap = new Map;
 for (let i = 0; i < n; i++) {
-  arrDic[arr[i]] = i;
+  myMap.set(uniqueArray[i], i);
 }
 
 let answer = "";
 for (let i = 0; i < n; i++) {
-  answer += arrDic[nList[i]] + ' ';
+  answer += myMap.get(arr[i]) + ' ';
 }
 
 console.log(answer);
