1f3fc28d284d5c8bc542f998c2cacb14db319c9e
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-10 23:42:41 +0900

    [Silver II] Title: 좌표 압축, Time: 2020 ms, Memory: 314380 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -4,31 +4,21 @@ const input = fs.readFileSync("/dev/stdin").toString().split('\n');
 let n = Number(input[0]);
 let nList = input[1].split(' ');
 
-let arr = []; // 원본 숫자 배열을 저장할 배열
-let arrSort = []; // 정렬할 숫자 배열을 저장할 배열
-
-// 숫자 리스트를 순회하며 숫자로 변환하여 arr와 arrSort에 추가
-for (let i = 0; i < n; i++) {
-  x = Number(nList[i]);
-  arr.push(x);
-  arrSort.push(x);
-}
-
 // 정렬된 배열에서 중복된 값을 제거하여 고유한 값만 남김
-arrSort = [...new Set(arrSort)];
+let arr = [...new Set(nList)].map(Number);
 
 // arrSort 배열을 오름차순으로 정렬
-arrSort.sort((a, b) => a - b);
+arr.sort((a, b) => a - b);
 
 // 각 숫자의 압축된 인덱스를 저장할 객체
 let arrDic = {};
 for (let i = 0; i < n; i++) {
-  arrDic[arrSort[i]] = i;
+  arrDic[arr[i]] = i;
 }
 
 let answer = "";
 for (let i = 0; i < n; i++) {
-  answer += arrDic[arr[i]] + ' ';
+  answer += arrDic[nList[i]] + ' ';
 }
 
 console.log(answer);
