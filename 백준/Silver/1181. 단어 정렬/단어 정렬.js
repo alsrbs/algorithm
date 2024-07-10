2bf609e8208ee018173320c1eb9c373264c491ed
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-10 20:56:09 +0900

    [Silver V] Title: 단어 정렬, Time: 4724 ms, Memory: 17024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -3,13 +3,11 @@ const input = fs.readFileSync("/dev/stdin").toString().split('\n');
 
 let n = Number(input[0]); // 단어의 개수
 let arr = [];
-let c = 0
 for (let i = 1; i <= n; i++) {
   let word = input[i];
   // 중복된 단어 제외
   if (!arr.includes(word)) {
     arr.push(word);
-    c++
   }
 }
 
@@ -26,7 +24,7 @@ arr.sort((a, b) => {
 
 
 answer = ""
-for (let i = 0; i < c; i++) {
+for (let i = 0; i < arr.length; i++) {
   answer += arr[i] + '\n';
 }
 
