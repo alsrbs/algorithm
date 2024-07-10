ab831c8e7e0357b95b0b2be270fee4cb420ff029
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-07-10 20:19:02 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 420 ms, Memory: 53240 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let n = Number(input[0]);
+let arr = [];
+
+for (let i = 1; i <= n; i++) {
+  let [a, b] = input[i].split(' ');
+  arr.push([Number(a), Number(b)]);
+}
+
+arr.sort((a, b) => { // x좌표 기준 오름차순 만일 같다면 y좌표 기준 오름차순
+  if (a[0] != b[0]) return a[0] - b[0]; 
+  else return a[1] - b[1]; 
+})
+
+let answer = "";
+for (let i = 0; i < n; i++) {
+  answer += arr[i][0] + " " + arr[i][1] + '\n';
+}
+
+console.log(answer);
