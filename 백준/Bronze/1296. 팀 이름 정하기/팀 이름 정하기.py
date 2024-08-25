06da884b3df5157f018743f2ee18f3f701f29647
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-08-25 22:28:23 +0900

    [Bronze I] Title: 팀 이름 정하기, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
+++ 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
@@ -1,8 +1,14 @@
 yeondu_name = input()
 n = int(input())
+
+name_list = []
+for i in range(n):
+    name_list.append(input())
+
+name_list = sorted(set(name_list))
+
 answer = [-1, 'zzz']
-for _ in range(n):
-    team_name = input()
+for team_name in name_list:
 
     L = yeondu_name.count('L') + team_name.count('L')
     O = yeondu_name.count('O') + team_name.count('O')
@@ -13,7 +19,5 @@ for _ in range(n):
     if answer[0] < res:
         answer[0] = res
         answer[1] = team_name
-    elif answer[0] == res:
-        answer[1] = min(answer[1], team_name)
 
 print(answer[1])
