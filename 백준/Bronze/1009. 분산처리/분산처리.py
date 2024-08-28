3c4c4eef4523daa3b5d191b0e9cbb2cc3c6a9080
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-08-28 23:10:42 +0900

    [Bronze II] Title: 분산처리, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,22 +1,13 @@
-T = int(input())
-
-for _ in range(T):
+n = int(input())
+for _ in range(n):
     a, b = map(int, input().split())
     a = a % 10
-    
+
     if a == 0:
         print(10)
-    elif a == 1 or a == 5 or a == 6:
+    elif a in [1, 5, 6]:
         print(a)
-    elif a == 4 or a == 9:
-        b = b % 2
-        if b == 1:
-            print(a)
-        else:
-            print((a * a) % 10)
+    elif a in [4, 9]:
+        print(a if b % 2 else a**2 % 10)
     else:
-        b = b % 4
-        if b == 0:
-            print((a**4) % 10 % 10 % 10)
-        else:
-            print((a**b) % 10 % 10 % 10)
+        print(a**(b % 4) % 10 if b % 4 else a**4 % 10)
