c4519d5dbdfe64a086ed5ff4495f27577467ca8c
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-09-02 00:13:52 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 40 ms, Memory: 34432 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -2,19 +2,16 @@ n = int(input())
 peaks = list(map(int, input().split()))
 
 answer = 0
-x = 0
-for i in range(n-1):
-    cnt = 0
-    for j in range(i+1, n):
-        if peaks[i] > peaks[j]:
-            cnt += 1
-        else:
-            x = j
-            break
-
+h = 0
+cnt = 0
+for i in range(n):
+    if h > peaks[i]:
+        cnt += 1
+    else:
+        h = peaks[i]
+        answer = max(cnt, answer)
+        cnt = 0
+else:
     answer = max(cnt, answer)
-
-    if x + answer >= n:
-        break
-
+    
 print(answer)
