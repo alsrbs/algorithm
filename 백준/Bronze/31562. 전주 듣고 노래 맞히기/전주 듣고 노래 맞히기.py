ef87ad8e5b5d9047d4e24bd18c512aa051cb8903
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-09-04 01:37:20 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
@@ -4,24 +4,19 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 
 dic_song = {}
-dic_cnt = {}
 for _ in range(n):
     song = input().split()
     sound = ''.join(song[2:5])
 
-    if sound not in dic_cnt:
-        dic_cnt[sound] = 1
+    if sound not in dic_song:
         dic_song[sound] = song[1]
-    elif sound in dic_cnt:
-        dic_cnt[sound] += 1
+    else:
         dic_song[sound] = '?'
 
 for _ in range(m):
     sound = ''.join(input().split())
 
-    if sound not in dic_song:
-        print('!')
-    elif dic_cnt[sound] >= 2:
-        print('?')
-    else:
+    if sound in dic_song:
         print(dic_song[sound])
+    else:
+        print('!')
