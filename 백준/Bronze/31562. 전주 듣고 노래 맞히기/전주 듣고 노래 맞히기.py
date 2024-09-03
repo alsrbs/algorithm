6723914a51e189965dfc6fb6044e2e8aca00a734
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-09-04 01:27:11 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
@@ -3,27 +3,25 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-dic = {}
+dic_song = {}
+dic_cnt = {}
 for _ in range(n):
     song = input().split()
-    dic[song[1]] = ''.join(song[2:5])
+    sound = ''.join(song[2:5])
+
+    if sound not in dic_cnt:
+        dic_cnt[sound] = 1
+        dic_song[sound] = song[1]
+    elif sound in dic_cnt:
+        dic_cnt[sound] += 1
+        dic_song[sound] = '?'
 
 for _ in range(m):
     sound = ''.join(input().split())
 
-    title = ''
-    cnt = 0
-    for k, v in dic.items():
-        if cnt >= 2:
-            break
-            
-        if sound == v:
-            cnt += 1
-            title = k
-
-    if cnt >= 2:
-        print('?')
-    elif cnt == 1:
-        print(title)
-    else:
+    if sound not in dic_song:
         print('!')
+    elif dic_cnt[sound] >= 2:
+        print('?')
+    else:
+        print(dic_song[sound])
