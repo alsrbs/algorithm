5bc37f0a25174d26b59f1940302cadf656a1964e
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-09-06 18:27:47 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 820 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -1,10 +1,7 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-answer = [0, 0]  # 학번, 같은 반이었던 학생 수
+answer = [0]*n
 
 for i in range(n):
     cnt = set()
@@ -13,9 +10,6 @@ for i in range(n):
             if arr[i][j] == arr[k][j]:
                 cnt.add(k)
 
-    # 같은 반이었던 학생 수가 더 많은 경우 업데이트
-    if answer[1] < len(cnt):
-        answer[1] = len(cnt)
-        answer[0] = i+1
+    answer[i] = len(cnt)
 
-print(answer[0])
+print(answer.index(max(answer))+1)
