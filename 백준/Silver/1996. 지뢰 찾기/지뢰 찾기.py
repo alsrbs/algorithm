c3ab80656cb011b06ed1b71f51079d4b62a58b74
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-09-08 10:57:07 +0900

    [Silver V] Title: 지뢰 찾기, Time: 1488 ms, Memory: 109668 KB -BaekjoonHub

---
+++ 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.py
@@ -0,0 +1,26 @@
+def count(r, c):
+    total = 0
+    for i in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+        nr = r + i[0]
+        nc = c + i[1]
+
+        if 0<= nr < n and 0<= nc < n and arr[nr][nc] != '.':
+            total += int(arr[nr][nc])
+
+    if total >= 10:
+        answer[r][c] = 'M'
+    else:
+        answer[r][c] = str(total)
+
+
+n = int(input())
+arr = [list(input()) for _ in range(n)]
+answer = [['*']*n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if '.' == arr[i][j]:
+            count(i, j)
+
+for i in answer:
+    print(''.join(i))
