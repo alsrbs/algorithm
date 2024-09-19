11be15987ca676a4a133c1271e69ec26518b2bbe
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-09-19 21:06:48 +0900

    [Silver IV] Title: 설탕 배달, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -3,19 +3,8 @@ const input = fs.readFileSync("/dev/stdin").toString();
 
 let n = Number(input);
 
-let result = NaN;
-let c = 0;
-while (n >= 0) {
-    if (n%5 === 0) {
-        result = n / 5 + c;
-        break; 
-    }
-    n -= 3;
-    c += 1;
-}
-
-if (result) {
-    console.log(result);
-} else {
+if ([1, 2, 4, 7].includes(n)) {
     console.log(-1);
+} else {
+    console.log(Math.floor(n / 5) + Math.floor((n % 5) / 3) + (n % 5) % 3);
 }
