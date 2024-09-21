ef19148be696e30c2fc333f09347d6b0f9174f40
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-09-21 15:58:52 +0900

    [Silver III] Title: 주유소, Time: 260 ms, Memory: 37464 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -6,14 +6,15 @@ const n = Number(input[0]);
 const roads = input[1].split(" ").map(Number);
 const citys = input[2].split(" ").map(Number);
 
-let current = citys[0];
-let result = citys[0] * roads[0];
+let current = BigInt(citys[0]);  // 첫 도시의 기름값
+let result = BigInt(0);  // 결과도 BigInt로 초기화
 
-for (let i = 1; i < n-1; i++) {
-    if (current > citys[i]) {
-        current = citys[i];
-    } 
-    result += current * roads[i];
+for (let i = 0; i < n - 1; i++) {
+    const cityPrice = BigInt(citys[i]);  // BigInt로 변환
+    if (current > cityPrice) {  
+        current = cityPrice;  // 더 저렴한 기름값으로 갱신
+    }
+    result += current * BigInt(roads[i]);  // 현재 기름값으로 해당 도로 비용 계산
 }
 
-console.log(result);
+console.log(result.toString());  // BigInt 값을 출력할 때는 .toString() 사용
