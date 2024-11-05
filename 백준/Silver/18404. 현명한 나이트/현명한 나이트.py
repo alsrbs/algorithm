b52ef34ad30fa60e9e7f73b357c1071814a5b88c
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-11-06 03:37:24 +0900

    [Silver I] Title: 현명한 나이트, Time: 920 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/18404. 현명한 나이트/현명한 나이트.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n, m = map(int, input().split())  # n 체스판의 크기 m 상대편 말의 수
+x, y = map(int, input().split())  # 나이트의 위치
+
+arr = [n*[0] for _ in range(n)]
+
+q = deque([(x-1, y-1)])
+
+dr = [-2, -2, -1, -1, 1, 1, 2, 2]
+dc = [-1, 1, -2, 2, -2, 2, -1, 1]
+
+while q:
+    r, c = q.popleft()
+
+    for i in range(8):
+        nr = r + dr[i]
+        nc = c + dc[i]
+
+        if nr < 0 or nr >= n or nc < 0 or nc >= n:
+            continue
+
+        if arr[nr][nc] != 0 or [x-1, y-1] == [nr, nc]:
+            continue
+
+        arr[nr][nc] = arr[r][c] + 1
+        q.append((nr, nc))
+
+for i in range(m):
+    a, b = map(int, input().split())
+    print(arr[a-1][b-1], end=' ')
