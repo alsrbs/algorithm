1d55ada7f5060124a50359493e44279843153b43
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-11-12 04:35:07 +0900

    [Silver III] Title: 십자카드 문제, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2659. 십자카드 문제/십자카드 문제.py
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.py
@@ -1,9 +1,8 @@
 from itertools import product
 
-# 입력 카드 값과 네 가지 회전 조합으로 시계수 생성
+# 카드 값을 입력받고, 회전을 통해 4가지 경우의 수로 구성
 cards = input().split()
-rotations = [''.join(cards[i:] + cards[:i]) for i in range(4)]
-clock_number = min(int(rotation) for rotation in rotations)
+rotations = sorted(int(''.join(cards[i:] + cards[:i])) for i in range(4))
 
 # 가능한 모든 시계수를 생성하고 중복을 제거하여 정렬
 nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -11,6 +10,4 @@ unique_clock_numbers = sorted(
     {min(int(''.join(p[i:] + p[:i])) for i in range(4)) for p in product(nums, repeat=4)}
 )
 
-# 시계수의 순서 찾기
-position = unique_clock_numbers.index(clock_number) + 1
-print(position)
+print(unique_clock_numbers.index(rotations[0]) + 1)
