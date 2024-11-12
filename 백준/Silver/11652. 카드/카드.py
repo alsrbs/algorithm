d87eeb23fbc78c3dc0a71cb8ef017182ed8f8641
alsrbs <122500158+alsrbs@users.noreply.github.com>
2024-11-12 20:50:31 +0900

    [Silver IV] Title: 카드, Time: 100 ms, Memory: 42900 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.py
+++ 백준/Silver/11652. 카드/카드.py
@@ -1,15 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 dic = {}
 
 # 숫자의 빈도 계산
 for _ in range(n):
     num = int(input())
-    if num in dic:
-        dic[num] += 1
-    else:
-        dic[num] = 1
+    dic[num] = dic.get(num, 0) + 1
 
-# 빈도수를 기준으로 내림차순 정렬하고, 빈도수가 같은 경우 숫자 오름차순으로 정렬
-sorted_dic = sorted(dic.items(), key=lambda x: (-x[1], x[0]))
+# 가장 큰 빈도수를 찾고, 빈도수가 같은 경우 가장 작은 숫자를 선택
+max_freq = max(dic.values())
+most_frequent_number = min(num for num, freq in dic.items() if freq == max_freq)
 
-print(sorted_dic[0][0])
+print(most_frequent_number)
